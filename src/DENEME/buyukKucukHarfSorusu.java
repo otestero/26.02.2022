55f04c7154694d238c11cebcdadb00ca7d103983
ömer <omerittester@gmail.com>
2022-04-05 00:02:48 +0300

    05.04.2022

---
+++ src/DENEME/buyukKucukHarfSorusu.java
@@ -0,0 +1,18 @@
+package DENEME;
+
+public class buyukKucukHarfSorusu {
+    public static void main(String[] args) {
+
+        String str= "ayhan beyhan";
+
+        System.out.println(str.replace("a", "A"));
+        System.out.println(str.replace("y", "Y"));
+
+        System.out.println(str.replaceAll("a", "A"));
+        System.out.println(str.replaceAll("y","Y"));
+
+        System.out.println(str.replace('a','A'));
+        System.out.println(str.replace('a','A'));
+
+    }
+}
